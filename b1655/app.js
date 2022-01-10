59d386ef8d87ac6c8a9fb5ea1a81eafc1bd5ce33
동길 이 <oio3215@naver.com>
2022-01-10 22:02:13 +0900

    01.10

--- b1655/app.js
+++ b1655/app.js
@@ -1,94 +1,115 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().split('\r\n');
+let input = fs.readFileSync(filePath).toString().trim();
 
-const N = input.shift();
-let Maxheap = [];
-let Minheap = [];
+let [n, ...arr] = input.split('\n').map((el) => Number(el));
 
-for(let i of input){
-    if(Maxheap.length === 0) heap_insert(Maxheap, Number(i), 0);
-    else{
-        if(Maxheap.length>Minheap.length) heap_insert(Minheap, Number(i), 1);
-        else heap_insert(Maxheap, Number(i), 0);
-
-        if(Maxheap[0]> Minheap[0]){
-            let mintop = heap_delete(Minheap, 1);
-            heap_insert(Minheap, heap_delete(Maxheap,0), 1);
-            heap_insert(Maxheap, mintop, 0);
-        }
+class Heap{
+    constructor(){
+        this.node = [];
     }
-    console.log(Maxheap[0]);
-}
 
-
-function heap_insert(heap, data, status){
-    let index = heap.length;
-
-    if(heap.length===0) heap.push(data);
-    else{
-        heap.push(data);
-        while(index > 0){
-            const parent = Math.floor((index-1)/2);
-            const child = index;
-
-            if (status === 0) { // 최대 힙일 때
-                if (heap[parent] < heap[child]) {
-                    [heap[parent], heap[child]] = [heap[child], heap[parent]];
-
-                    index = parent;
-                } else {
-                    break;
-                }
-            }else{ // 최소 힙일 때
-                if (heap[parent] > heap[child]) {
-                    [heap[parent], heap[child]] = [heap[child], heap[parent]];
-
-                    index = parent;
-                } else {
-                    break;
-                }
-            }
-        }
-    }
-}
-
-function heap_delete(heap, status){
-    const head = heap[0];
-    heap[0] = heap[heap.length-1];
-    heap.pop();
-    let index = 0;
-
-    while(index<heap.length){
-        const child = (index+1)*2;
-        const parent = index;
+    insert(data, status){
+        this.node.push(data);
+        let child = this.node.length-1;
         
-        if(status === 0){ /// 최대힙 일때
-            if(heap[parent] < heap[child]){ // 오른쪽 자식
-                [heap[parent], heap[child]] = [heap[child], heap[parent]];
-    
-                index = child;
-            }else if(heap[parent] < heap[child-1]){ // 왼쪽 자식
-                [heap[parent], heap[child-1]] = [heap[child-1], heap[parent]];
-    
-                index = child-1;
-            }else{
-                break;
+        if(status === 'max'){
+            while(child !== 0){
+                let parent = Math.floor((child-1)/2);
+                
+                if(this.node[parent] < this.node[child]){
+                    this.swap(parent,child);
+                    child=parent;
+                } else {
+                    break;
+                }
             }
-        }else{ /// 최소힙 일때
-            if (heap[parent] > heap[child]) { // 오른쪽 자식
-                [heap[parent], heap[child]] = [heap[child], heap[parent]];
-
-                index = child;
-            } else if (heap[parent] > heap[child - 1]) { // 왼쪽 자식
-                [heap[parent], heap[child - 1]] = [heap[child - 1], heap[parent]];
-
-                index = child - 1;
-            } else {
-                break;
+        }else{
+            while(child !== 0){
+                let parent = Math.floor((child-1)/2);
+                
+                if(this.node[parent] > this.node[child]){
+                    this.swap(parent,child);
+                    child=parent;
+                } else {
+                    break;
+                }
             }
         }
     }
 
-    return head;
+    delete(status){
+        if(this.node.length===1) return this.node.pop();
+
+        let topNode = this.node[0];
+
+        this.node[0] = this.node.pop();
+
+        let parent = 0;
+
+        if(status === 'max'){
+            while (true) {
+                const leftchild = parent * 2 + 1;
+                const rightchild = parent * 2 + 2;
+                let currentNode = parent;
+
+                // Left
+                if (this.node[leftchild] && this.node[leftchild] > this.node[currentNode]) currentNode = leftchild;
+
+                if (this.node[rightchild] && this.node[rightchild] > this.node[currentNode]) currentNode = rightchild;
+
+                if (currentNode !== parent) {
+                    this.swap(parent, currentNode);
+                    parent = currentNode;
+                } else break;
+            }
+        }else{
+            while (true) {
+                const leftchild = parent * 2 + 1;
+                const rightchild = parent * 2 + 2;
+                let currentNode = parent;
+
+                // Left
+                if (this.node[leftchild] !== undefined && this.node[leftchild] < this.node[currentNode]) currentNode = leftchild;
+
+                if (this.node[rightchild] !== undefined && this.node[rightchild] < this.node[currentNode]) currentNode = rightchild;
+
+                if (currentNode !== parent) {
+                    this.swap(parent, currentNode);
+                    parent = currentNode;
+                } else break;
+            }
+        }
+
+        return topNode;
+    }
+
+    swap(y, x){
+        [this.node[y], this.node[x]] = [this.node[x], this.node[y]];
+    }
 }
+
+const maxHeap = new Heap();
+const minHeap = new Heap();
+const answer = [];
+
+for(let i of arr){
+    if(maxHeap.node.length === minHeap.node.length){
+        maxHeap.insert(i, 'max');
+    }else{
+        minHeap.insert(i, 'min');
+    }
+
+    if(minHeap.node.length === 0) answer.push(maxHeap.node[0]);
+    else {
+        if(maxHeap.node[0] > minHeap.node[0]){
+            const temp = minHeap.delete('min');
+            const temp2 = maxHeap.delete('max');
+            minHeap.insert(temp2, 'min');
+            maxHeap.insert(temp, 'max');
+        }
+        answer.push(maxHeap.node[0]);
+    }
+}
+
+console.log(answer.join('\n'));
--- b1927/app.js
+++ b1927/app.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(filePath).toString().trim();
 
 const [n, ...arr] = input.split('\n').map((s) => parseInt(s));
 
-class maxHeap{
+class minHeap{
     constructor(){
         this.heap = [];
     }
@@ -14,7 +14,7 @@ class maxHeap{
         let child = this.heap.length-1;
         let parent = Math.floor((child-1)/2);
         
-        while(this.heap[parent] < this.heap[child]){
+        while(this.heap[parent] > this.heap[child]){
             this.swap(parent, child);
             child = parent;
             parent = Math.floor((child-1)/2);
@@ -31,14 +31,13 @@ class maxHeap{
         let parent = 0;
         let child = parent*2 + 1; //왼쪽 자식 기준
 
-        // if 오른쪽 자식이 있고, 오른쪽 자식이 왼쪽 자식보다 더 크면 +1 해줌.
-        if(this.heap[child+1] && this.heap[child] < this.heap[child+1]) child++;
+        if(this.heap[child+1] && this.heap[child] > this.heap[child+1]) child++;
 
-        while(this.heap[child] && this.heap[parent] < this.heap[child]){
+        while(this.heap[child] && this.heap[parent] > this.heap[child]){
             this.swap(parent, child);
             parent = child;
             child = parent * 2 + 1;
-            if (this.heap[child+1] && this.heap[child] < this.heap[child+1]) child++;
+            if (this.heap[child+1] && this.heap[child] > this.heap[child+1]) child++;
         }
 
         return head;
@@ -49,7 +48,7 @@ class maxHeap{
     }
 }
 
-const mh = new maxHeap();
+const mh = new minHeap();
 
 let answer = [];
 
