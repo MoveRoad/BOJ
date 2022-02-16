26eb5dbe518656602df4316bdbcf62359ffaac92
동길 이 <oio3215@naver.com>
2022-02-16 23:32:58 +0900

    02.16

---
+++ 백준/b14681/app.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const X = input[0];
+const Y = input[1];
+
+if (X > 0) {
+  console.log(Y > 0 ? 1 : 4);
+} else {
+  console.log(Y > 0 ? 2 : 3);
+}
