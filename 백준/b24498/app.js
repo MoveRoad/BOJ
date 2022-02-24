425ddb546b7abd7865de3737be9a76e84404d285
동길 이 <oio3215@naver.com>
2022-02-24 23:03:08 +0900

    02.24

--- 백준/b24498/app.js
+++ 백준/b24498/app.js
@@ -5,7 +5,7 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const N = input[0];
 const block = input[1].split(" ").map(Number);
 
-let answer = [block[0], block[block.length - 1]];
+const answer = [block[0], block[block.length - 1]];
 for (let i = 1; i < block.length - 1; i++) {
   answer.push(Math.min(block[i - 1], block[i + 1]) + block[i]);
 }
