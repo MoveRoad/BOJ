4aabd6662293bd8d4de5f64576e9e68bc84bdf8c
동길 이 <oio3215@naver.com>
2022-02-24 22:42:07 +0900

    02.24

---
+++ 백준/b24498/app.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = input[0];
+const block = input[1].split(" ").map(Number);
+
+let answer = [block[0], block[block.length - 1]];
+for (let i = 1; i < block.length - 1; i++) {
+  answer.push(Math.min(block[i - 1], block[i + 1]) + block[i]);
+}
+
+console.log(Math.max(...answer));
